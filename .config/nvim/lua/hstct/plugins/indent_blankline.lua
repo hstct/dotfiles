52eef5e9c65a085eb276b2f74f715d60954df324
hstct <code@elmc.at>
2022-09-13 18:09:22 +0200

    change(nvim): indentlines now exclude more filetypes

--- .config/nvim/lua/hstct/plugins/indent_blankline.lua
+++ .config/nvim/lua/hstct/plugins/indent_blankline.lua
@@ -1,5 +1,19 @@
 require("indent_blankline").setup({
   show_first_indent_level = false,
-  filetype_exclude = { "help", "packer", "FTerm" },
+  filetype_exclude = {
+    "help",
+    "packer",
+    "FTerm",
+    "NeogitStatus",
+    "NeogitCommitView",
+    "NeogitLogView",
+    "NeogitPopup",
+    "NeogitCommitMessage",
+    "man",
+    "TelescopePrompt",
+    "DiffviewFiles",
+    "DiffviewFileHistory",
+    "fugitive",
+  },
   buftype_exclude = { "terminal", "nofile" },
 })
