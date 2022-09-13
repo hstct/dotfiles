fb2db04949e69d6a1e2db774bd6948efd0efe6a3
hstct <code@elmc.at>
2022-09-13 20:27:01 +0200

    change(nvim): extend `nvim-autopairs` settings

--- .config/nvim/lua/hstct/plugins/nvim_autopairs.lua
+++ .config/nvim/lua/hstct/plugins/nvim_autopairs.lua
@@ -1,4 +1,17 @@
-require('nvim-autopairs').setup()
+require("nvim-autopairs").setup({
+  disable_filetype = { "TelescopePrompt" },
+  break_line_filetype = nil,
+  html_break_line_filetype = {
+    "html",
+    "vue",
+    "typescriptreact",
+    "svelte",
+    "javascriptreact",
+  },
+  ignored_next_char = "[%w%%%'%[%\"%.]",
+  enable_check_bracket_line = false,
+  check_ts = true,
+})
 
 -- integration w/ nvim-cmp
-require('cmp').event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
+require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
