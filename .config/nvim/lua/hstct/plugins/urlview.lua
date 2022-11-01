b2e2337a807c1feaea731cdbff45d1b6ca9147a1
hstct <code@elmc.at>
2022-11-01 14:21:36 +0100

    chore(nvim): update urlview setup

--- .config/nvim/lua/hstct/plugins/urlview.lua
+++ .config/nvim/lua/hstct/plugins/urlview.lua
@@ -1,8 +1,9 @@
-require("urlview").setup({
-  custom_searches = {
-    jira = {
-      capture = "OR%-%d+",
-      format = "https://network.atix.de/browse/%s",
-    },
-  },
+require("urlview").setup()
+
+local search = require("urlview.search")
+local search_helpers = require("urlview.search.helpers")
+
+search["jira"] = search_helpers.generate_custom_search({
+  capture = "OR%-%d+",
+  format = "https://network.atix.de/browse/%s",
 })
