68bac0173e85e41dcf033dd2149eea43e99d2a12
hstct <code@elmc.at>
2024-02-04 20:27:37 +0100

    change(nvim): rm harpoon

--- .config/nvim/lua/hstct/plugins/telescope.lua
+++ .config/nvim/lua/hstct/plugins/telescope.lua
@@ -5,7 +5,6 @@ return {
             "nvim-telescope/telescope-fzf-native.nvim",
             build = "make",
         },
-        "ThePrimeagen/harpoon",
     },
     init = function()
         local tb = require("telescope.builtin")
@@ -22,15 +21,6 @@ return {
         u.map("n", "<leader>fk", tb.keymaps, { desc = "telescope: keymaps" })
         u.map("n", "<leader>H", tb.help_tags, { desc = "telescope: help tags" })
 
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
-
-        u.map("n", "<C-h>", mark.add_file, { desc = "harpoon: add file" })
-        u.map("n", "<C-e>", ui.toggle_quick_menu, { desc = "harpoon: toggle quick menu" })
-        u.map("n", "'1", function() ui.nav_file(1) end, { desc = "harpoon: nav file 1" })
-        u.map("n", "'2", function() ui.nav_file(2) end, { desc = "harpoon: nav file 2" })
-        u.map("n", "'3", function() ui.nav_file(3) end, { desc = "harpoon: nav file 3" })
-        u.map("n", "'4", function() ui.nav_file(4) end, { desc = "harpoon: nav file 4" })
     end,
     config = function()
         local t = require("telescope")
@@ -67,8 +57,5 @@ return {
             },
         }
         t.load_extension("fzf")
-        t.load_extension("harpoon")
-
-        require("harpoon").setup()
     end,
 }
