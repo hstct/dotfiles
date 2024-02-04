8590dcccaf3a1f44c200fc25e7644affb471102c
hstct <code@elmc.at>
2024-02-04 20:32:25 +0100

    feat(nvim): add oil.nvim

---
+++ .config/nvim/lua/hstct/plugins/files.lua
@@ -0,0 +1,50 @@
+return {
+    "stevearc/oil.nvim",
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("oil").setup({
+            default_file_explorer = true,
+            columns = {
+                "icon",
+            },
+            buf_options = {
+                buflisted = false,
+                bufhidden = "hide",
+            },
+            win_options = {
+                wrap = false,
+                signcolumn = "no",
+                cursorcolumn = false,
+                foldcolumn = "0",
+                spell = false,
+                list = false,
+                conceallevel = 2,
+                concealcursor = "n",
+            },
+            restore_win_options = true,
+            skip_confirm_for_simple_edits = false,
+            delete_to_trash = false,
+            trash_command = "trash-put",
+            prompt_save_on_select_new_entry = true,
+            keymaps = {
+                ["g?"] = "actions.show_help",
+                ["<CR>"] = "actions.select",
+                ["<C-s>"] = "actions.select_vsplit",
+                ["<C-h>"] = "actions.select_split",
+                ["<C-t>"] = "actions.select_tab",
+                ["<C-p>"] = "actions.preview",
+                ["<C-c>"] = "actions.close",
+                ["<C-l>"] = "actions.refresh",
+                ["-"] = "actions.parent",
+                ["_"] = "actions.open_cwd",
+                ["`"] = "actions.cd",
+                ["~"] = "actions.tcd",
+                ["g."] = "actions.toggle_hidden",
+            },
+            use_default_keymaps = true,
+        })
+        local u = require("hstct.util.functions")
+        u.map("n", "-", "<cmd>Oil<CR>", { desc = "oil: open parent directory "})
+    end,
+}
