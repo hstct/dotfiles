d051d7aca8143eb7adaa9edcb650179a5d499831
hstct <code@elmc.at>
2024-02-04 20:24:30 +0100

    chore(qutebrowser): update ua

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -16,7 +16,7 @@ c.tabs.title.format_pinned = "{index}: {audio}{current_title}"
 # general
 c.auto_save.session = True
 c.content.default_encoding = "utf-8"
-c.content.javascript.can_access_clipboard = True
+c.content.javascript.clipboard = "access"
 c.content.notifications.enabled = True
 c.editor.command = ["kitty", "nvim", "{}"]
 c.fileselect.handler = "external"
@@ -33,7 +33,7 @@ c.tabs.mousewheel_switching = False
 c.content.cookies.accept = "no-3rdparty"
 c.content.webrtc_ip_handling_policy = "default-public-interface-only"
 c.content.site_specific_quirks.enabled = False
-c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5790.170 Safari/537.36"
+c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6167.139 Safari/537.36"
 
 # urls
 c.url.searchengines = {
