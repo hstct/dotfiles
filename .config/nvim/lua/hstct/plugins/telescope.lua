1ce0a7b90ad3a9df11681716e03ac4312b98f664
hstct <code@elmc.at>
2024-02-04 20:30:08 +0100

    feat(nvim): replace zknvim with wiki.vim

--- .config/nvim/lua/hstct/plugins/telescope.lua
+++ .config/nvim/lua/hstct/plugins/telescope.lua
@@ -8,7 +8,75 @@ return {
     },
     init = function()
         local tb = require("telescope.builtin")
+        local ta = require("telescope.actions")
+        local tas = require("telescope.actions.state")
         local u = require("hstct.util.functions")
+
+        local function files_wiki()
+            tb.find_files({
+                prompt_title = "Wiki files",
+                cwd = '~/.local/wiki',
+                disable_devicons = true,
+                find_command = { "rg", "--files", "--sort", "path" },
+                file_ignore_patterns = {
+                    "%journal/",
+                    "%.stversions/",
+                    "%.git/",
+                },
+                path_display = function(_, path)
+                    local name = path:match "(.+)%.[^.]+$"
+                    return name or path
+                end,
+                attach_mappings = function(prompt_bufnr, _)
+                    ta.select_default:replace_if(function()
+                        return tas.get_selected_entry() == nil
+                    end, function()
+                        ta.close(prompt_bufnr)
+
+                        local new_name = tas.get_current_line()
+                        if new_name == nil or new_name == "" then
+                            return
+                        end
+
+                        vim.fn["wiki#page#open"](new_name)
+                    end)
+
+                    return true
+                end,
+            })
+        end
+
+        local function files_journal()
+            tb.find_files({
+                prompt_title = "Journal files",
+                cwd = "~/.local/wiki/journal",
+                disable_devicons = true,
+                find_command = { "rg", "--files", "--sort", "path" },
+                file_ignore_patterns = {
+                    "%.stversions/",
+                    "%.git/",
+                },
+                path_display = function(_, path)
+                    local name = path:match "(.+)%.[^.]+$"
+                    return name or path
+                end,
+                attach_mappings = function(prompt_bufnr, _)
+                    ta.select_default:replace_if(function()
+                        return tas.get_selected_entry() == nil
+                    end, function()
+                        ta.close(prompt_bufnr)
+                        local new_name = tas.get_current_line()
+                        if new_name == nil or new_name == "" then
+                            return
+                        end
+
+                        vim.fn["wiki#page#open"](new_name)
+                    end)
+                    return true
+                end,
+            })
+        end
+
         u.map("n", "<C-p>", function()
             local ok = pcall(tb.git_files, { show_untracked = true })
             if not ok then
@@ -20,6 +88,8 @@ return {
         u.map("n", "<leader>fg", tb.git_status, { desc = "telescope: git status" })
         u.map("n", "<leader>fk", tb.keymaps, { desc = "telescope: keymaps" })
         u.map("n", "<leader>H", tb.help_tags, { desc = "telescope: help tags" })
+        u.map("n", "<leader>zp", files_wiki, { desc = "telescope: wiki files" })
+        u.map("n", "<leader>zj", files_journal, { desc = "telescope: journal files" })
 
     end,
     config = function()
--- .config/nvim/lua/hstct/plugins/wiki.lua
+++ .config/nvim/lua/hstct/plugins/wiki.lua
@@ -1,72 +1,27 @@
 return {
-    "mickael-menu/zk-nvim",
-    config = function()
-        local zk = require("zk")
-        local util = require("zk.util")
-        local commands = require("zk.commands")
-        local u = require("hstct.util.functions")
-
-        local function make_edit_fn(defaults, picker_options)
-            return function(options)
-                options = vim.tbl_extend("force", defaults, options or {})
-                zk.edit(options, picker_options)
-            end
-        end
-
-        local function make_new_fn(defaults)
-            return function(options)
-                options = vim.tbl_extend("force", defaults, options or {})
-                zk.new(options)
-            end
-        end
-
-        local function make_new_prompt_fn()
-            return function()
-                vim.ui.input({ prompt = "Note Title: " }, function(title)
-                    zk.new({ title = title, dir = "records" })
-                end, "New Note")
-            end
-        end
-
-        zk.setup({
-            picker = "telescope",
-            lsp = {
-                config = {
-                    cmd = { "zk", "lsp" },
-                    name = "zk",
-                },
-                auto_attach = {
-                    enabled = true,
-                    filetypes = { "markdown" },
-                },
-            },
-        })
-
-        commands.add("ZkOrphans", make_edit_fn({ orphan = true }, { title = "Zk Orphans" }))
-        commands.add("ZkRecents", make_edit_fn({ createdAfter = "2 weeks ago" }, { title = "Zk Recents" }))
-        commands.add("ZkLiveGrep", function(options)
-            options = options or {}
-            local notebook_path = options.notebook_path or util.resolve_notebook_path(0)
-            local notebook_root = util.notebook_root(notebook_path)
-            if notebook_root then
-                require("telescope.builtin").live_grep({ cwd = notebook_root, prompt_title = "Zk Live Grep" })
-            else
-                vim.notify("No notebook found", vim.log.levels.ERROR)
-            end
-        end)
-        commands.add("ZkNewNote", make_new_prompt_fn())
-        commands.add("ZkNewJournal", make_new_fn({ dir = "journal" }))
-        commands.add("ZkJournal", make_edit_fn({ hrefs = { "journal" }, sort = { "created" } }, { title = "Zk Journal" }))
-
-        u.map("n", "<leader>zn", "<cmd>ZkNewNote<CR>", { desc = "zk: new note" })
-        u.map("n", "<leader>zf", "<cmd>ZkLiveGrep<CR>", { desc = "zk: live grep " })
-        u.map("n", "<leader>zp", "<cmd>ZkNotes { sort = { 'modified' }, tags = { 'wiki' } }<CR>", { desc = "zk: find notes" })
-        u.map("n", "<leader>fw", "<cmd>ZkNotes { sort = { 'modified' }, tags = { 'wiki' } }<CR>", { desc = "zk: find notes" })
-        u.map("n", "<leader>zo", "<cmd>ZkNotes { sort = { 'modified' }, match = vim.fn.input('Search: '), tags = { 'wiki' } }<CR>", { desc = "zk: find specific notes" })
-        u.map("n", "<leader>zj", "<cmd>ZkNotes { sort = { 'modified' }, tags = { 'journal' } }<CR>", { desc = "zk: find journal notes" })
-        u.map("n", "<leader>zb", "<cmd>ZkBacklinks<CR>", { desc = "zk: find backlinks" })
-        u.map("n", "<leader>zl", "<cmd>ZkLinks<CR>", { desc = "zk: find links" })
-        u.map("n", "<leader>zd", "<cmd>ZkNewJournal<CR>", { desc = "zk: new daily note" })
-        u.map("n", "<leader>zt", "<cmd>ZkTags<CR>", { desc = "zk: find tags" })
-    end,
+    {
+        "lervag/wiki.vim",
+        cond = vim.loop.cwd() ~= "/home/hstct/Wiki/dolmenwood",
+        init = function()
+            vim.g.wiki_root = "~/.local/wiki"
+            vim.g.wiki_filetypes = { "md" }
+            vim.g.wiki_mappings_local = {
+                ["<plug>(wiki-link-transform-operator)"] = "gL",
+            }
+            vim.g.wiki_toc_depth = 2
+            vim.g.wiki_ui_method = {
+                confirm = "nvim",
+                input = "nvim",
+                select = "nvim",
+            }
+            vim.g.wiki_templates = {
+                {
+                    match_func = function(ctx)
+                        return ctx.path:find "journal/"
+                    end,
+                    source_filename = "~/.local/wiki/.templates/journal.md"
+                }
+            }
+        end,
+    },
 }
