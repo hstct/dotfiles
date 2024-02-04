8afad3c6bb7d91e92b9f473a27b54af69daed49b
hstct <code@elmc.at>
2024-02-04 20:30:43 +0100

    change(nvim): extra markdown settings

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -1,1 +1,6 @@
 vim.opt_local.conceallevel = 2
+vim.opt_local.textwidth = 0
+vim.opt_local.linebreak = true
+
+local u = require("hstct.util.functions")
+u.map("n", "<leader>sc", "<cmd>lua vim.opt_local.conceallevel = 2<CR>", { desc = "markdown: reset conceallevel "})
